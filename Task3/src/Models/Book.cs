70a593c10a6eb7b9be02d1d81d1e555ed83fb76b
skels417 <ivan.ivan.parsaev@inbox.ru>
2025-02-17 13:09:45 +0700

    Add files via upload

---
+++ Task3/src/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task3.Models
+{
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public string Year { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public Book(string author, string title, string isbn, string year)
+        {
+            Author = author;
+            Title = title;
+            ISBN = isbn;
+            Year = year;
+            CreateDate = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {Title}, Author: {Author}, ISBN: {ISBN}, Year: {Year}, Added: {CreateDate}";
+        }
+    }
+}
