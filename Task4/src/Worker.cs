b563ed53d479a1d2a218cb7c0904eb032e62cc47
skels417 <ivan.ivan.parsaev@inbox.ru>
2025-02-17 13:12:52 +0700

    Add files via upload

---
+++ Task4/src/Worker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+
+namespace Task4
+{
+    public class Worker
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfEmployment { get; set; }
+        public string Grade { get; set; }
+
+        public Worker(string name, int age, DateTime dateOfEmployment, string grade)
+        {
+            Name = name;
+            Age = age;
+            DateOfEmployment = dateOfEmployment;
+            Grade = grade;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Date of Employment: {DateOfEmployment.ToShortDateString()}, Grade: {Grade}";
+        }
+    }
+}
