da9c153a686e3b43939f2593257bcc2c91478759
skels417 <ivan.ivan.parsaev@inbox.ru>
2025-03-19 21:47:21 +0700

    Add files via upload

---
+++ Task11/src/Migrations/202503190931546_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿namespace FortuneTellerApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Predictions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Text = c.String(),
+                        Author = c.String(),
+                        CreatedAt = c.DateTime(nullable: false),
+                        Category = c.Int(nullable: false),
+                        Likes = c.Int(nullable: false),
+                        Dislikes = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        PredictionCount = c.Int(nullable: false),
+                        LastPredictionDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.Predictions");
+        }
+    }
+}
